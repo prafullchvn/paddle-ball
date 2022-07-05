e6dbc3f62c581bbb38e910bc904fd2bcfa5b618e
Prafull Chavan <99485204+prafullchvn@users.noreply.github.com>
2022-07-05 13:16:07 +0530

    Reflecting ball when hit with paddle.;

--- ball.js
+++ ball.js
@@ -33,65 +33,111 @@
       this.speed.dy = -this.speed.dy
     }
 
-    hitTheBorder({ height, width }) {
-      const { x, y } = this.position;
-      if (x > width - this.size || x < 0) this.invertXDelta();
-      if (y > height - this.size || y < 0) this.invertYDelta();
+    calculateCenter() {
+      const c1 = this.position.x + this.size;
+      const c2 = this.position.y + this.size;
+      return [c1, c2];
     }
+
+    isInBall(ball) {
+      const [x2, y2] = ball.calculateCenter();
+      const [x1, y1] = this.calculateCenter();
+
+      const r1 = Math.round(this.size / 2);
+      const r2 = Math.round(ball.size / 2);
+
+      if (Math.hypot((y2 - y1), (x2 - x1)) <= (r1 + r2)) {
+        return true;
+      }
+      return false;
+    }
+
+    hitTheBorder(obstacle) {
+      const { x: ballX, y: ballY } = this.position;
+      const { height: ballHeight, width: ballWidth } = this.size;
+      const { position: { x: viewX, y: viewY } } = obstacle
+      const { size: { height: viewHeight, width: viewWidth } } = obstacle;
+
+      if (ballX <= viewX || (ballX + ballWidth) >= (viewX + viewWidth)) {
+        this.invertXDelta();
+      }
+      if (ballY <= viewY || (ballY + ballHeight) >= (viewY + viewHeight)) {
+        this.invertYDelta();
+      }
+    }
+
   }
 
   class Paddle {
-    constructor(id, size, position, dx) {
+    constructor(id, position, size, speed) {
       this.id = id;
-      this.size = size;
       this.position = position;
-      this.dx = dx;
-    }
-
-    moveLeft() {
-      this.position.x -= this.dx;
+      this.size = size;
+      this.speed;
     }
 
     moveRight() {
-      this.position.x += this.dx;
+      this.position.x += this.speed.dx;
     }
 
-    isCrossingLeftBorder() {
-      return (this.position.x - this.dx) < 0;
+    moveLeft() {
+      this.position.x -= this.speed.dx;
     }
 
-    isCrossingRightBorder(width) {
-      return (this.position.x + this.size.width + this.dx) > width;
+    withinRightBorderOf(view) {
+      const newX = this.position.x + this.speed.x;
+      return newX >= view.position.x;
     }
 
-    getInfo() {
-      const { height, width } = this.size;
-      const { x, y } = this.position;
+    withinLeftBorderOf(view) {
+      const newX = this.position.x - this.speed.x;
+      return newX <= view.position.x;
+    }
 
-      return {
-        id: this.id,
-        size: { height, width },
-        position: { x, y },
-        dx: this.dx
-      };
+    isCollidingWith(ball) {
+      const ballInfo = ball.getInfo();
+      const { x: paddleX, y: paddleY } = this.position;
+      const { width: paddleWidth } = this.size;
+      const { x: ballX, y: ballY } = ballInfo.position;
+      const { height: ballHeight, width: ballWidth } = ballInfo.size;
+
+      const midX = (ballX + ballX + ballWidth) / 2;
+      const midY = (ballY + ballY + ballHeight + ballHeight) / 2;
+      if (midY >= paddleY) {
+        if (midX >= paddleX && midX <= (paddleX + paddleWidth)) {
+          ball.invertYDelta();
+        }
+      }
+    }
+  }
+
+  const checkCollision = (ball, paddle) => {
+    const ballInfo = ball.getInfo();
+    const { x: paddleX, y: paddleY } = paddle.position;
+    const { width: paddleWidth } = paddle.size;
+    const { x: ballX, y: ballY } = ballInfo.position;
+    const { height: ballHeight, width: ballWidth } = ballInfo.size;
+
+    const midX = (ballX + ballX + ballWidth) / 2;
+    const midY = (ballY + ballY + ballHeight + ballHeight) / 2;
+    if (midY >= paddleY) {
+      if (midX >= paddleX && midX <= (paddleX + paddleWidth)) {
+        ball.invertYDelta();
+      }
     }
   }
 
   const createBalls = () => {
-    const ball1 = new Ball('ball-1', { x: 30, y: 30 }, { dx: 5, dy: 2 }, 30, 'red');
-    const ball2 = new Ball('ball-2', { x: 30, y: 20 }, { dx: 5, dy: 4 }, 30, 'green');
-    const ball3 = new Ball('ball-3', { x: 30, y: 50 }, { dx: 5, dy: 3 }, 30, 'yellow');
-    const ball4 = new Ball('ball-4', { x: 10, y: 30 }, { dx: 5, dy: 6 }, 30, 'orange');
-    const ball5 = new Ball('ball-5', { x: 50, y: 40 }, { dx: 5, dy: 7 }, 30, 'pink');
-    return [ball1, ball2, ball3, ball4, ball5];
+    const ball1 = new Ball('ball-1', { x: 210, y: 210 }, { dx: -3, dy: 5 }, { height: 30, width: 30 }, 'red');
+    return [ball1];
   };
 
   const px = value => value + 'px';
 
-  const drawBall = (viewElement, ball) => {
+  const drawBall = (ball) => {
     const {
       id,
-      size,
+      size: { height },
       color,
       position: { x, y },
     } = ball.getInfo();
@@ -101,22 +147,30 @@
     ballElement.style.position = 'absolute';
     ballElement.style.top = px(y);
     ballElement.style.left = px(x);
-    ballElement.style.width = px(size);
+    ballElement.style.width = px(height);
     ballElement.style.borderRadius = '50%';
     ballElement.style.aspectRatio = '1';
     ballElement.style.backgroundColor = color;
 
-    viewElement.appendChild(ballElement);
+    document.querySelector('body').appendChild(ballElement);
   };
 
-  const drawView = ({ top, left, width, height }) => {
+  const updateBall = (ball) => {
+    const { id, position: { x, y }, } = ball.getInfo();
+    const ballElement = document.getElementById(id);
+
+    ballElement.style.top = px(y);
+    ballElement.style.left = px(x);
+  };
+
+  const drawView = ({ position: { x, y }, size: { width, height } }) => {
     const view = document.createElement('div');
     view.id = 'view';
     view.style.width = px(width);
     view.style.height = px(height);
-    view.style.position = 'absolute';
-    view.style.top = px(top);
-    view.style.left = px(left);
+    // view.style.position = 'absolute';
+    view.style.top = px(x);
+    view.style.left = px(y);
     view.style.border = '1px solid black';
     view.style.position = 'relative';
 
@@ -124,67 +178,96 @@
     return view;
   };
 
-  const drawPaddle = (viewElement, paddle) => {
+  const drawPaddle = (paddle, viewElement) => {
+    const { id, size: { width, height }, position: { x, y } } = paddle;
+
     const paddleElement = document.createElement('div');
-    const { id, size: { height, width }, position: { x, y } } =
-      paddle.getInfo();
 
     paddleElement.id = id;
-    paddleElement.style.height = px(height);
-    paddleElement.style.width = px(width);
     paddleElement.style.position = 'absolute';
+    paddleElement.style.width = px(width);
+    paddleElement.style.height = px(height);
     paddleElement.style.top = px(y);
     paddleElement.style.left = px(x);
     paddleElement.style.border = '1px solid black';
 
-    viewElement.appendChild(paddleElement);
-    return paddleElement;
+    document.querySelector('body').appendChild(paddleElement);
   }
 
-  const updateBall = (ball) => {
-    const { id, position: { x, y }, } = ball.getInfo();
-    const ballElement = document.getElementById(id);
+  const isCrossingBottomBorder = (ball, view) => {
+    const ballInfo = ball.getInfo();
+    const { y: ballY } = ballInfo.position;
+    const { height: ballHeight } = ballInfo.size;
+    const { y: viewY } = view.position;
+    const { height: viewHeight } = view.size;
 
-    ballElement.style.top = px(x);
-    ballElement.style.left = px(y);
-  };
+    if ((ballY + ballHeight) >= (viewY + viewHeight))
+      return true;
+    return false;
+  }
 
-  const updatePaddle = (paddle) => {
-    const { position: { x }, id } = paddle.getInfo();
-    const paddleElement = document.getElementById(id);
-    paddleElement.style.left = px(x);
-  };
-
-  const registerKeyDownEvent = (paddle, { width }) => {
-    document.onkeydown = (event) => {
-      const key = event.key;
-      if (key === 'ArrowRight' && !paddle.isCrossingRightBorder(width)) {
-        paddle.moveRight();
-      }
-      if (key === 'ArrowLeft' && !paddle.isCrossingLeftBorder(width)) {
-        paddle.moveLeft();
-      }
-      updatePaddle(paddle);
+  const movePaddleToRight = (paddle, view) => {
+    const newPaddleX = paddle.position.x + paddle.speed.dx;
+    const paddleWidth = paddle.size.width;
+    if ((newPaddleX + paddleWidth) <= (view.position.y + view.size.width)) {
+      paddle.position.x += paddle.speed.dx;
     }
-  };
+
+    const paddleElement = document.getElementById(paddle.id);
+    paddleElement.style.left = px(paddle.position.x);
+  }
+
+  const movePaddleToLeft = (paddle, view) => {
+    const newPaddleX = paddle.position.x - paddle.speed.dx;
+    if (newPaddleX >= view.position.x) {
+      paddle.position.x -= paddle.speed.dx;
+    }
+
+    const paddleElement = document.getElementById(paddle.id);
+    paddleElement.style.left = px(paddle.position.x);
+  }
+
+  const registerPaddleEvent = (paddle, view) => {
+
+    document.addEventListener('keydown', event => {
+      if (event.key === 'ArrowRight') {
+        movePaddleToRight(paddle, view);
+      }
+      if (event.key === 'ArrowLeft') {
+        movePaddleToLeft(paddle, view);
+      }
+    });
+  }
 
   const main = () => {
-    const view = { top: 200, left: 200, width: 600, height: 600 };
+    const view = {
+      position: { x: 200, y: 200 },
+      size: { width: 600, height: 600 }
+    };
+    const paddle = { id: 'paddle', position: { x: 200, y: 760 }, size: { width: 100, height: 40 }, speed: { dx: 25, dy: 0 } };
+
+
     const balls = createBalls();
-
-    const paddleSize = { height: 20, width: 100 };
-    const paddlePosition = { x: 0, y: view.height - paddleSize.height };
-    const paddle = new Paddle('p-1', paddleSize, paddlePosition, 25);
-
     const viewElement = drawView(view);
-    balls.forEach((ball) => drawBall(viewElement, ball));
-    drawPaddle(viewElement, paddle);
-    registerKeyDownEvent(paddle, view);
+    balls.forEach((ball) => drawBall(ball));
 
-    setInterval(() => {
+    drawPaddle(paddle, viewElement);
+    registerPaddleEvent(paddle, view);
+
+    const intervalId = setInterval(() => {
       balls.forEach((ball) => ball.move());
       balls.forEach(updateBall);
       balls.forEach((ball) => ball.hitTheBorder(view));
+      balls.forEach(ball => checkCollision(ball, paddle));
+
+      const ballsCrossed = balls.filter(
+        (ball) => isCrossingBottomBorder(ball, view)
+      );
+
+      if (ballsCrossed.length > 0) {
+        console.log('lost')
+        clearInterval(intervalId);
+      }
     }, 30);
   };
 
